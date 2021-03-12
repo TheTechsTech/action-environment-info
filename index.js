665d2661118afb3d411908f9da8feaff0e6c126b
techno-express <technoexpressnet@gmail.com>
2021-03-12 14:34:52 -0500

    add ability to get OS version for runners

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const os = require('os');
 const core = require('@actions/core');
 
 try {
   core.setOutput("platform", process.platform);
   core.setOutput("arch", process.arch);
+  core.setOutput("release", os.release());
 } catch (error) {
   core.setFailed(error.message);
 }
