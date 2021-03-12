359b9612baef97182b1c8c1dd80f6b3f8cda4450
techno-express <technoexpressnet@gmail.com>
2021-03-12 14:59:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const os = require('os');
 const core = require('@actions/core');
 
 try {
+  let version = os.release();
   core.setOutput("platform", process.platform);
   core.setOutput("arch", process.arch);
-  core.setOutput("release", os.release());
+  core.setOutput("release", version);
 } catch (error) {
   core.setFailed(error.message);
 }
