10b79b347691f0bd92ec5b8f4104cd3dcb20ac06
techno-express <technoexpressnet@gmail.com>
2021-03-12 16:18:17 -0500

    fix to get actual macOS version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const os = require('os');
 const core = require('@actions/core');
+const macosRelease = require('macos-release');
 
 try {
-  let version = os.release();
+  let version = (process.platform === 'darwin') ? macosRelease(version).version : os.release();
   core.setOutput("platform", process.platform);
   core.setOutput("arch", process.arch);
   core.setOutput("release", version);
